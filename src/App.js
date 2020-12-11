886267a551a00e5939e8877ce2d13d7d6d35f01f
Teo Marcelo <tmarcelojr@gmail.com>
2020-12-11 08:20:12 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ registerUsername, setRegisterUsername ] = useState('');
+	const [ registerPassword, setRegisterPassword ] = useState('');
+	const [ loginUsername, setLoginUsername ] = useState('');
+	const [ loginPassword, setLoginPassword ] = useState('');
+
+  const register = () => {
+    axios({
+      method: 'POST',
+      data: {
+        username: registerUsername,
+        password: registerPassword
+      },
+      withCredentials: true,
+      url: 'http://localhost:4000/register'
+    })
+    .then(res => console.log(res))
+  }
+
+  const login = () => {
+    axios({
+      method: 'POST',
+      data: {
+        username: loginUsername,
+        password: loginPassword
+      },
+      withCredentials: true,
+      url: 'http://localhost:4000/login'
+    })
+    .then(res => console.log(res))
+  }
+
+  const getUser = () => {
+    axios({
+      method: 'GET',
+      withCredentials: true,
+      url: 'http://localhost:4000/getUser'
+    })
+    .then(res => console.log(res))
+  }
+
+	return (
+		<div className="App">
+			<div>
+				<h1>Register</h1>
+				<input placeholder="username" onChange={(e) => setRegisterUsername(e.target.value)} type="text" />
+				<input placeholder="password" onChange={(e) => setRegisterPassword(e.target.value)} type="text" />
+				<button onClick={register} >Submit</button>
+			</div>
+
+			<div>
+				<h1>Login</h1>
+				<input placeholder="username" onChange={(e) => setLoginUsername(e.target.value)} type="text" />
+				<input placeholder="password" onChange={(e) => setLoginPassword(e.target.value)} type="text" />
+				<button onClick={login} >Submit</button>
+			</div>
+
+			<div>
+				<h1>Get User</h1>
+				<button onClick={getUser}>Submit</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
